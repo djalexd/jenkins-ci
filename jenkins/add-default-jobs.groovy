6714f546c90bc3b085b29c05c4c294e4452840d8
Alex Dobjanschi <alex.dobjanschi@gmail.com>
2015-12-16 09:36:17 +0200

    default jobs using dsl

--- jenkins/add-default-jobs.groovy
+++ jenkins/add-default-jobs.groovy
@@ -6,22 +6,81 @@
 import jenkins.model.*
 import groovy.io.FileType
 
-/*
- * TODO Magic value - path location: /usr/share/jenkins/ref/default-jobs
- */
+job("branch-auto-configurer") {
+    displayName "Project branch auto-configurer"
+    keepDependencies(false)
+    disabled(false)
+    parameters {
+        stringParam("project", null, "Base name of this job, e.g. project name")
+        stringParam("git_scm_url", null, "Git SCM URL of this job")
+    }
+    scm {
+        git {
+            remote {
+                github('djalexd/jenkins-job-templates', 'ssh')
+                credentials('2c1fa93e-c1fd-407c-b771-aa724d20ffa6')
+            }
+            clean()
+            relativeTargetDir('jenkins-job-templates')
+        }
+    }
+    steps {
+        dsl {
+            text(readFileFromWorkspace('jenkins-job-templates/branch-auto-configurer.groovy'))
+            removeAction('DISABLE')
+            ignoreExisting()
+        }
+    }
+}
 
-def jobPath = "/usr/share/jenkins/ref/default-jobs"
+job("default-java-pipeline-factory") {
+    displayName "Default Java pipeline factory"
+    keepDependencies(false)
+    disabled(false)
+    parameters {
+        stringParam("project", null, "Base name of this job, e.g. project name")
+        stringParam("git_scm_url", null, "Git SCM URL of this job")
+    }
+    scm {
+        git {
+            remote {
+                github('djalexd/jenkins-job-templates', 'ssh')
+                credentials('2c1fa93e-c1fd-407c-b771-aa724d20ffa6')
+            }
+            clean()
+            relativeTargetDir('jenkins-job-templates')
+        }
+    }
+    steps {
+        dsl {
+            text(readFileFromWorkspace('jenkins-job-templates/pipeline-factory.groovy'))
+            removeAction('DISABLE')
+            ignoreExisting()
+        }
+    }
+}
 
-new File(jobPath).eachFile() { file ->
-    def jobName = file.getName()
-    def jobContentsAsXml = file.getText("UTF-8")
-
-    println "About to create job named " + jobName
-
-	def xmlStream = new ByteArrayInputStream(jobContentsAsXml.getBytes())
-	try {
-		Jenkins.instance.createProjectFromXML(jobName, xmlStream)
-	} catch (Exception e) {
-		println "Unable to create job named " + jobName + ". A probable reason is that job already exists"
-	}
-}  
+job("pipeline-generator") {
+    keepDependencies(false)
+    disabled(false)
+    parameters {
+        stringParam("prefix", null, "Job prefix")
+    }
+    scm {
+        git {
+            remote {
+                github('djalexd/jenkins-job-templates', 'ssh')
+                credentials('2c1fa93e-c1fd-407c-b771-aa724d20ffa6')
+            }
+            clean()
+            relativeTargetDir('jenkins-job-templates')
+        }
+    }
+    steps {
+        dsl {
+            text(readFileFromWorkspace('jenkins-job-templates/pipeline-generator.groovy'))
+            removeAction('DISABLE')
+            ignoreExisting()
+        }
+    }
+}
